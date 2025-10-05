e6e500eab37d8945e9306745ea53cdb26ae76426
stachuman <cgpsmapper@gmail.com>
2025-10-05 20:39:53 +0200

    0.2.6

--- backend/app/config.py
+++ backend/app/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
 
     # Application
     APP_NAME: str = "einkpdf"
-    APP_VERSION: str = "0.2.4"
+    APP_VERSION: str = "0.2.6"
     DEBUG: bool = False
 
     # Database
